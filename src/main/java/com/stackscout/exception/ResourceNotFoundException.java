84cbdd9140c508627a5a0f366b52b10768099a1e
LINESKL <priveth06@gmail.com>
2026-01-31 01:18:28 +0500

    1

--- src/main/java/com/stackscout/exception/ResourceNotFoundException.java
+++ src/main/java/com/stackscout/exception/ResourceNotFoundException.java
@@ -1,5 +1,15 @@
 package com.stackscout.exception;
 
-public class ResourceNotFoundException {
+/**
+ * Исключение, которое выбрасывается когда ресурс не найден
+ */
+public class ResourceNotFoundException extends RuntimeException {
     
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
